f4f5bed9d9a7e0e2d68b088bae9ef5e006480a09
Djair Soares <djhanlj@gmail.com>
2021-06-03 22:11:12 -0300

    implementação do envio do arquvo.csv

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+     const formData = new FormData();
+
+     const file = uploadedFiles[0];
+     formData.append('file', file.file, file.name)
 
     // TODO
 
     try {
-      // await api.post('/transactions/import', data);
+       await api.post('/transactions/import', formData);
+       history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+       console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploadFiles = files.map(file => ({
+      file, 
+      name: file.name,
+      readableSize: filesize(file.size)
+    }));
+
+    setUploadedFiles(uploadFiles);
   }
 
   return (
